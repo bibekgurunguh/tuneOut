46b2936407e03979664a0c4c3bd138b8f90a2933
Hough-Lab <hough@outlook.com>
2021-01-13 20:10:42 +0000

    webpack.config added

--- client/src/index-js/index-options.js
+++ client/src/index-js/index-options.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index-css/index-options.css';
-import Options from './components/Options';
+import Options from '../components/options/Options';
 
 ReactDOM.render(
   <React.StrictMode>
     <Options/>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('options')
 );
 
