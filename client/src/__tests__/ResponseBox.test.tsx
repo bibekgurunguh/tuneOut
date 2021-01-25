04414b24df4a8211921da03939a6498ca595b690
James <jmp1g16@gmail.com>
2021-01-25 10:09:28 +0000

    Tests: test cases created for response box and snapshot test for responseitem

--- client/src/__tests__/ResponseBox.test.tsx
+++ client/src/__tests__/ResponseBox.test.tsx
@@ -6,19 +6,72 @@ import {
   waitForElement,
 } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import mockSongInfo from '../../__mocks__/mocks';
+import '@testing-library/jest-dom';
+import 'jest-canvas-mock';
+
+import { mockSongInfo } from '../../__mocks__/mocks';
 
 import ResponseItem from '../components/popup/components/ResponseItem';
+import { ResponseBox } from '../components/popup/components/ResponseBox';
 
-describe('<ResponseItem /> 1', () => {
-  test('matches snapshot', () => {
-    const tree = render(<ResponseItem item="" attribute="" />);
+describe('<ResponseBox /> should render various elements of the song result correctly', () => {
+  beforeEach(() => {
+    render(
+      <ResponseBox
+        animation={false}
+        setSongInfo={''}
+        songInfo={JSON.stringify(mockSongInfo)}
+      ></ResponseBox>,
+    );
+  });
+  it('should render the trackID correctly', () => {
+    const trackId = screen.getByText('Woods');
+    expect(trackId).toBeInTheDocument();
+  });
+  it('should render the Artist correctly', () => {
+    const trackArtist = screen.getByText('&ME');
+    expect(trackArtist).toBeInTheDocument();
+  });
+  it('should render the release date correctly', () => {
+    const trackReleased = screen.getByText('2015-07-10');
+    expect(trackReleased).toBeInTheDocument();
+  });
+  it('should render the album title correctly', () => {
+    const albumTitle = screen.getByText('Trilogy');
+    expect(albumTitle).toBeInTheDocument();
+  });
+  it('should render the Label correctly', () => {
+    const artistLabel = screen.getByText('Keinemusik');
+    expect(artistLabel).toBeInTheDocument();
+  });
+  it('should render all elements together', () => {
+    const trackId = screen.getByText('Woods');
+    const trackArtist = screen.getByText('&ME');
+    const trackReleased = screen.getByText('2015-07-10');
+    const albumTitle = screen.getByText('Trilogy');
+    const artistLabel = screen.getByText('Keinemusik');
+
+    expect(trackId).toBeInTheDocument();
+    expect(trackArtist).toBeInTheDocument();
+    expect(trackReleased).toBeInTheDocument();
+    expect(albumTitle).toBeInTheDocument();
+    expect(artistLabel).toBeInTheDocument();
+  });
+  test('rendered ResponseBox matches the existing snapshot when songInfo is provided', () => {
+    const tree = render(
+      <ResponseBox
+        songInfo={JSON.stringify(mockSongInfo)}
+        setSongInfo=""
+        animation="false"
+      />,
+    );
     expect(tree).toMatchSnapshot();
   });
+});
 
-  test('should display a blank login form, with remember me checked by default', async () => {
-    await render(<ResponseItem item="testitem" attribute="testattribute" />);
+describe('<ResponseItem />', () => {
+  test('should render responseItem to match previous snapshot', () => {
+    const tree = render(<ResponseItem item="trackId" attribute="Woods" />);
+    expect(tree).toMatchSnapshot();
   });
 });
-// describe('<ResponseItem /> 2', () => {
-// });
--- client/src/components/popup/components/ResponseBox.tsx
+++ client/src/components/popup/components/ResponseBox.tsx
@@ -1,40 +1,33 @@
 import * as React from 'react';
-import ResponseItem from './ResponseItem'
-import './ResponseBox.css'
+import ResponseItem from './ResponseItem';
+import './ResponseBox.css';
 import Lottie from 'react-lottie';
 // import animationData from '../animations/loading-animation.json'
-const animationData = require("../animations/loading-animation.json");
-import YtSearch from './external-links/YtSearch'
-import SpotifySearch from './external-links/SpotifySearch'
-import DiscogsSearch from './external-links/DiscogsSearch'
+const animationData = require('../animations/loading-animation.json');
+import YtSearch from './external-links/YtSearch';
+import SpotifySearch from './external-links/SpotifySearch';
+import DiscogsSearch from './external-links/DiscogsSearch';
 
 export function ResponseBox({ songInfo, setSongInfo, animation }) {
-
-
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
+      preserveAspectRatio: 'xMidYMid slice',
+    },
   };
 
   if (animation === true) {
     return (
       <div className={'animation'}>
-        <Lottie
-          options={defaultOptions}
-          height={260}
-          width={275}
-          />
-    </div>
-    )
+        <Lottie options={defaultOptions} height={260} width={275} />
+      </div>
+    );
   }
 
   if (songInfo) {
-
-    const songObj = JSON.parse(songInfo)
+    const songObj = JSON.parse(songInfo);
     if (songObj.status.code === 1001) {
       //? on song identification failure
       songInfo = [];
@@ -43,7 +36,7 @@ export function ResponseBox({ songInfo, setSongInfo, animation }) {
           <h3 className={'errorMessage'}>couldn't identify track</h3>
           <h3 className={'errorMessage'}>please try again</h3>
         </div>
-      )
+      );
     } else if (songObj.status.code === 2004) {
       //? on song fingerprint failure
       songInfo = [];
@@ -52,37 +45,36 @@ export function ResponseBox({ songInfo, setSongInfo, animation }) {
           <h3 className={'errorMessage'}>couldn't detect audio</h3>
           <h3 className={'errorMessage'}>please try again</h3>
         </div>
-      )
+      );
     }
 
-    console.log('parser', songObj)
-    console.log('typeofparsed', typeof songObj)
-    console.log('songinfo.metadata', songObj.metadata)
+    // console.log('parser', songObj)
+    // console.log('typeofparsed', typeof songObj)
+    // console.log('songinfo.metadata', songObj.metadata)
 
+    const id = songObj.metadata.music[0];
 
-    const id = songObj.metadata.music[0]
+    const title = id.title;
+    const artist = id.artists[0].name;
+    const label = id.label;
+    const released = id.release_date;
+    const album = id.album.name;
 
-    const title = id.title
-    const artist = id.artists[0].name
-    const label = id.label
-    const released = id.release_date
-    const album = id.album.name
-
-    const trackId = 'Track ID: '
-    const trackArtist = 'Artist: '
-    const trackReleased = 'Released: '
-    const trackAlbum = 'Album: '
-    const trackLabel = 'Label: '
+    const trackId = 'Track ID: ';
+    const trackArtist = 'Artist: ';
+    const trackReleased = 'Released: ';
+    const trackAlbum = 'Album: ';
+    const trackLabel = 'Label: ';
 
     let externalIdSpotify;
     let externalIdYoutube;
 
     if (id.external_metadata.spotify) {
-        externalIdSpotify = id.external_metadata.spotify.track.id
+      externalIdSpotify = id.external_metadata.spotify.track.id;
     }
 
     if (id.external_metadata.youtube) {
-        externalIdYoutube = id.external_metadata.youtube.vid
+      externalIdYoutube = id.external_metadata.youtube.vid;
     }
 
     //? on song identification successful
@@ -94,27 +86,34 @@ export function ResponseBox({ songInfo, setSongInfo, animation }) {
         <ResponseItem attribute={trackAlbum} item={album}></ResponseItem>
         <ResponseItem attribute={trackLabel} item={label}></ResponseItem>
         <div className={'linkbox'}>
-          <div className='yt'>
-            <YtSearch externalIdYoutube={externalIdYoutube} title={title} artist={artist}></YtSearch>
+          <div className="yt">
+            <YtSearch
+              externalIdYoutube={externalIdYoutube}
+              title={title}
+              artist={artist}
+            ></YtSearch>
           </div>
-          <div className='spotify'>
-            <SpotifySearch externalIdSpotify={externalIdSpotify} title={title} artist={artist} ></SpotifySearch>
+          <div className="spotify">
+            <SpotifySearch
+              externalIdSpotify={externalIdSpotify}
+              title={title}
+              artist={artist}
+            ></SpotifySearch>
           </div>
           <DiscogsSearch artist={artist} album={album}></DiscogsSearch>
         </div>
       </div>
-    )
+    );
   }
 
-
   //? On popup initialization
-  const firstMsg = 'navigate to an audible tab'
-  const secondMsg = 'click identify to start'
+  const firstMsg = 'navigate to an audible tab';
+  const secondMsg = 'click identify to start';
 
   return (
     <div className={'emptyResBox'}>
       <ResponseItem attribute={firstMsg} item={''}></ResponseItem>
       <ResponseItem attribute={secondMsg} item={''}></ResponseItem>
     </div>
-  )
+  );
 }
