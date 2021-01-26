3be6eff3efa61278a93f69145efb8457e5181835
James <jmp1g16@gmail.com>
2021-01-26 11:59:10 +0000

    Refactor: Added Types to utils folder and index-background and renamed functions in utils

--- client/src/utils/apiRequestFunctions.ts
+++ client/src/utils/apiRequestFunctions.ts
@@ -1,64 +1,96 @@
 // const url = require('url');
 
 import FormData from 'form-data';
-import * as fetch from 'node-fetch';
-
+import fetch from 'node-fetch';
 
 const { createHmac } = require('crypto');
-// const request = require('request');
 const axios = require('axios');
-// let FormData = require('form-data');
-// let fetch = require('node-fetch');
 
-// const FormData = require ('form-data')
+interface ApiOptions {
+  host: string;
+  endpoint: string;
+  signature_version: string;
+  data_type: string;
+  secure: boolean;
+  access_key: string;
+  access_secret: string;
+}
 
-export const defaultOptions = {
+export const defaultApiOptions: ApiOptions = {
   host: 'identify-eu-west-1.acrcloud.com',
   endpoint: '/v1/identify',
   signature_version: '1',
-  data_type:'audio',
+  data_type: 'audio',
   secure: true,
   access_key: 'a735916ac5e523565ecf5a2d872ac541',
-  access_secret: 'QO3uR9K1GC626FjTLADGvQ7wWYWlzciJEs8VukbU' //! Store in .env file??
+  access_secret: 'QO3uR9K1GC626FjTLADGvQ7wWYWlzciJEs8VukbU', //! Store in .env file??
 };
 
-export function buildStringToSign(method, uri, accessKey, dataType, signatureVersion, timestamp) {
-  return [method, uri, accessKey, dataType, signatureVersion, timestamp].join('\n');
-}
+export const concatenateToSignatureString = (
+  method: string,
+  uri: string,
+  accessKey: string,
+  dataType: string,
+  signatureVersion: string,
+  timestamp: number,
+) => {
+  return [method, uri, accessKey, dataType, signatureVersion, timestamp].join(
+    '\n',
+  );
+};
 
-export function sign(signString, accessSecret) {
+export const createEncodedHashedSignature = (
+  signString: string,
+  accessSecret: string,
+): string => {
   return createHmac('sha1', accessSecret)
     .update(Buffer.from(signString, 'utf-8'))
 
-    .digest().toString('base64');
-}
+    .digest()
+    .toString('base64');
+};
 
+export const requestSongInfoUsingBlob = (
+  data: Blob,
+  options: ApiOptions,
+  cb: Function,
+) => {
+  const timestamp = new Date().getTime() / 1000;
 
-export function identify_v2(data, options, cb) {
+  const signatureString = concatenateToSignatureString(
+    'POST',
+    options.endpoint,
+    options.access_key,
+    options.data_type,
+    options.signature_version,
+    timestamp,
+  );
 
-  const timestamp = new Date().getTime()/1000;
-
-  const stringToSign = buildStringToSign('POST',
-      options.endpoint,
-      options.access_key,
-      options.data_type,
-      options.signature_version,
-      timestamp);
-
-  const signature = sign(stringToSign, options.access_secret);
+  const signature = createEncodedHashedSignature(
+    signatureString,
+    options.access_secret,
+  );
 
   const form = new FormData();
   form.append('sample', data);
-  form.append('sample_bytes', data.length);
+  form.append('sample_bytes', data.size);
   form.append('access_key', options.access_key);
   form.append('data_type', options.data_type);
   form.append('signature_version', options.signature_version);
   form.append('signature', signature);
   form.append('timestamp', timestamp);
 
-  fetch("http://"+options.host + options.endpoint,
-      {method: 'POST', body: form })
-      .then((res) => {return res.text()})
-      .then((res) => {cb(res, null)})
-      .catch((err) => {cb(null, err)});
-}
+  fetch('http://' + options.host + options.endpoint, {
+    method: 'POST',
+    body: form,
+  })
+    .then((res) => {
+      return res.text();
+    })
+    .then((res) => {
+      cb(res, null);
+    })
+    .catch((err) => {
+      cb(null, err);
+    });
+};
